7a7548ea5e4e91dc6a72ed2b0401c4013170c3a2
Userlet <letitia259@yahoo.com>
2018-07-10 16:39:23 +0300

    Loaded static resources

--- onlineshopping/src/main/java/elu/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/elu/onlineshopping/controller/PageController.java
@@ -1,6 +1,7 @@
 package elu.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,4 +14,14 @@ public class PageController {
 		mv.addObject("greeting", "Hello from Spring MVC");
 		return mv;
 	}
+/*	@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting) {
+		if(greeting == null) {
+			greeting="Hello !";
+		}
+		ModelAndView mv= new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}*/
+	
 }
